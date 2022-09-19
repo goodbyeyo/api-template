71d4dbf3a35fa02e75e6026c0b2bbdaa87f301d6
Sangwook Hwang <goodbyeyo@naver.com>
2022-09-19 23:43:48 +0900

    Kakao 로그인 구현 진행중

---
+++ src/main/java/template/web/kakao/client/KakaoTokenClient.java
@@ -0,0 +1,16 @@
+package template.web.kakao.client;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.cloud.openfeign.SpringQueryMap;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import template.web.kakao.dto.KakaoTokenDto;
+
+@FeignClient(url = "https://kauth.kakao.com", name = "kakaoTokenClient")
+public interface KakaoTokenClient {
+
+    @PostMapping(value = "/oauth/token", consumes = "application/json")
+    KakaoTokenDto.Response requestKakaoToken(@RequestHeader("Content-Type") String contentType,
+                                             @SpringQueryMap KakaoTokenDto.Request request);
+
+}
---
+++ src/main/java/template/web/kakao/controller/KakaoTokenController.java
@@ -0,0 +1,46 @@
+package template.web.kakao.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import template.web.kakao.client.KakaoTokenClient;
+import template.web.kakao.dto.KakaoTokenDto;
+
+@Controller
+@RequiredArgsConstructor
+public class KakaoTokenController {
+
+    private final KakaoTokenClient kakaoTokenClient;
+
+    @Value("${kakao.client.id")
+    private String clientId;
+
+    @Value("${kakao.client.secret")
+    private String clientSecret;
+
+    private static final String contentType = "application/x-www-form-urlencoded;charset=utf-8";
+    private static final String grantType = "authorization_code";
+
+    @Value("${kakao.client.redirectUrl")
+    private final String redirectUrl;
+
+    @GetMapping("/login")
+    public String login() {
+        return "loginForm";
+    }
+
+    @GetMapping("/oauth/kakao/callback")
+    public @ResponseBody String loginCallBack(String code) {
+        KakaoTokenDto.Request requestDto = KakaoTokenDto.Request.builder()
+                .client_id(clientId)
+                .client_secret(clientSecret)
+                .grant_type(grantType)
+                .code(code)
+                .redirect_uri(redirectUrl)
+                .build();
+        KakaoTokenDto.Response kakaoToken = kakaoTokenClient.requestKakaoToken(contentType, requestDto);
+        return "kakao token : " + kakaoToken ;
+    }
+}
