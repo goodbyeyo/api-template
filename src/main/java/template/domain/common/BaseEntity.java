532fcbd0374af00d79811480568c2662d0b72d22
Sangwook Hwang <yoonseol1983@gmail.com>
2022-09-20 23:58:56 +0900

    Entity 생성

---
+++ src/main/java/template/domain/common/BaseEntity.java
@@ -0,0 +1,23 @@
+package template.domain.common;
+
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.Column;
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+
+@EntityListeners(value = {AuditingEntityListener.class})
+@MappedSuperclass   // 자식클래스에서 테이블에 맵핑
+@Getter
+public abstract class BaseEntity {
+
+    @CreatedBy
+    @Column(updatable = false)
+    private String createdBy;
+
+    @LastModifiedBy
+    private String modifiedBy;
+}
